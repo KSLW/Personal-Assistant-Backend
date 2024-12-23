2611ddba9a90172a2dc2687904ed7e86c66097bd
Kirk SL Wilkinson <KSLW1997@outlook.com>
2024-12-23 13:08:05 +0000

    Update
    Updated firebase services import

--- app/main.py
+++ app/main.py
@@ -6,8 +6,7 @@ from firebase_admin import auth
 from firebase_admin.auth import verify_id_token
 from pydantic import BaseModel
 
-from app.services.firestore_service import (add_reminder, add_task,
-                                            get_reminders, update_reminder)
+from app.services.firestore_service import add_reminder, add_task, get_reminders, update_reminder
 
 app = FastAPI(
     title="Personal Assistant Backend API",
